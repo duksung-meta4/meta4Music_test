87cdbe31efc5d4e1f9a5be9a33d1543cb3dbc23a
jeein2222 <jiinbag057@gmail.com>
2022-05-19 14:08:47 +0900

    python test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+import  { React, useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [lyricInput, setLyricInput] = useState("");
+    const [result, setResult] = useState();
+  
+    async function onSubmit(event) {
+      // event.preventDefault();
+  
+      const response=await axios.get('http://localhost:8080');
+      console.log(response);
+      console.log(response.data);
+
+      setResult();
+      setLyricInput("");
+    }
+    
+    useEffect(()=>{
+      onSubmit();
+    });
+
+    
+
+    return (
+        <div className="App">
+            <h3>Type your Lyrics</h3>
+            <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                name="lyric"
+                placeholder="Enter your lyric"
+                value={lyricInput}
+                onChange={(e) => setLyricInput(e.target.value)}
+            />
+            <input type="submit" value="Generate lyrics" />
+            </form>
+            <div>{result}</div>
+        </div>
+    );
 }
 
 export default App;
